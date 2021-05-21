fddd87ccbcb0cec4f6c72af0231c53d19fbefab2
ArsMen1 <71522524+ArsMen1@users.noreply.github.com>
2021-05-21 21:40:14 +0300

    Добавил слушателя н аблоки ответов и цвет в зависимости от выбора+массив ответов пользователя

--- LogicsAndStyles/Test.js
+++ LogicsAndStyles/Test.js
@@ -1,20 +1,30 @@
 const questionText = document.querySelector(".question"); //Блок вопроса
 const answerText = document.querySelector(".answers"); // Блок ответов
 
-let arrAnswers = Object.values(arrCard.card1.answers); //Массив ответов
+let arrAnswers = Object.values(arrCard.card1.answers); //Массив ответов из изходника
+let yourAnswers = []; //Массив ваших ответов
 
-//выводим на экран Вопрос
-questionText.innerHTML = arrCard.card1.question;
+questionText.innerHTML = arrCard.card1.question; //выводим на экран Вопрос
 
 arrAnswers.sort(() => Math.random() - 0.5); //Перемешаем массив что бы  нельзя быол запомнить положние ответов
 
 //Для каждого ответа сделать блок с индефикатором
-arrAnswers.forEach((item, index, array) => {
+arrAnswers.forEach((elem) => {
   let divs = document.createElement("div"); //создаем блок
-  divs.append(item); //присваеваем блоку значение из массива
+  divs.append(elem); //присваеваем блоку значение из массива
   divs.classList.add("hoverAnswer"); // присваеваем блокам класс для стилизации
   answerText.appendChild(divs); // добавляем блок на экран
 });
 
-const answe22rText = document.querySelectorAll(".hoverAnswer"); // Блок ответов
-console.log(arrAnswers);
+document.querySelector(".answers").onclick = answerClick; //Нa выбранный блок вешаем событие
+function answerClick() {
+  let r = event.target.innerHTML;
+  if (r == arrCard.card1.answers.right) {
+    event.target.classList.add("rightAnswer");
+  } else {
+    event.target.classList.add("falseAnswer");
+  }
+  yourAnswers.push(r);
+}
+
+console.log(yourAnswers);

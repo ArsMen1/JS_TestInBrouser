6d9d8aefcd8777c155dc858866647907cca45fb6
ArsMen1 <71522524+ArsMen1@users.noreply.github.com>
2021-05-24 23:17:24 +0300

    Запрет горячих клавишь которые позволяют вызвать Chrome DevTools

--- Setting.js
+++ Setting.js
@@ -77,3 +77,21 @@ let arrCard = {
     },
   },
 };
+
+//Запрет на открытие Chrome DevTools
+document.oncontextmenu = new Function("return false;");
+
+document.onkeydown = function (e) {
+  if (event.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
